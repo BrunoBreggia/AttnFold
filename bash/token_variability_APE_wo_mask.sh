ba26a69e8e7dbc3dae8e0372f02e2488973ee8da
Bruno Breggia <breggiabruno@gmail.com>
2026-05-29 14:06:29 -0300

    Bash experiments updated

---
+++ bash/token_variability_APE_wo_mask.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+# Experimentos de Token Variability con Absolute Positional Encoding (TV_APE) con seed determinado por argumento ($1)
+data=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10")
+
+# loop for 32 dim embedding
+exps=(1 2 3 4 5 6 7 8 9 10)
+
+for i in "${!exps[@]}"
+do
+    python main.py train-attention "sample/train_tanda_17_${data[$i]}.csv" "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}" --pos_enc absolute --k_bias --emb_dim 32 --seed $1 --epochs 100
+    python main.py test-attention "sample/train_tanda_17_${data[$i]}.csv" "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}"
+    python sample/visualization.py "17_${data[$i]}" "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}"
+    mv "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}/matrices" "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}/matrices_epochs100"
+    mv "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}/test_tanda_17_${data[$i]}.pdf" "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}/test_17_${data[$i]}_epochs100.pdf"
+done
+
+
+# loop for 64 dim embedding
+exps=(11 12 13 14 15 16 17 18 19 20)
+
+for i in "${!exps[@]}"
+do
+    python main.py train-attention "sample/train_tanda_17_${data[$i]}.csv" "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}" --pos_enc absolute --k_bias --emb_dim 64 --seed $1 --epochs 100
+    python main.py test-attention "sample/train_tanda_17_${data[$i]}.csv" "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}"
+    python sample/visualization.py "17_${data[$i]}" "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}"
+    mv "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}/matrices" "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}/matrices_epochs100"
+    mv "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}/test_tanda_17_${data[$i]}.pdf" "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}/test_17_${data[$i]}_epochs100.pdf"
+done
+
+
+# loop for 128 dim embedding
+exps=(21 22 23 24 25 26 27 28 29 30)
+
+for i in "${!exps[@]}"
+do
+    python main.py train-attention "sample/train_tanda_17_${data[$i]}.csv" "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}" --pos_enc absolute --k_bias --emb_dim 128 --seed $1 --epochs 100
+    python main.py test-attention "sample/train_tanda_17_${data[$i]}.csv" "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}"
+    python sample/visualization.py "17_${data[$i]}" "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}"
+    mv "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}/matrices" "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}/matrices_epochs100"
+    mv "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}/test_tanda_17_${data[$i]}.pdf" "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}/test_17_${data[$i]}_epochs100.pdf"
+done
+
+
+# loop for 256 dim embedding
+exps=(31 32 33 34 35 36 37 38 39 40)
+
+for i in "${!exps[@]}"
+do
+    python main.py train-attention "sample/train_tanda_17_${data[$i]}.csv" "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}" --pos_enc absolute --k_bias --emb_dim 256 --seed $1 --epochs 100
+    python main.py test-attention "sample/train_tanda_17_${data[$i]}.csv" "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}"
+    python sample/visualization.py "17_${data[$i]}" "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}"
+    mv "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}/matrices" "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}/matrices_epochs100"
+    mv "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}/test_tanda_17_${data[$i]}.pdf" "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}/test_17_${data[$i]}_epochs100.pdf"
+done
+
+
+# loop for 512 dim embedding
+exps=(41 42 43 44 45 46 47 48 49 50)
+
+for i in "${!exps[@]}"
+do
+    python main.py train-attention "sample/train_tanda_17_${data[$i]}.csv" "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}" --pos_enc absolute --k_bias --emb_dim 512 --seed $1 --epochs 100
+    python main.py test-attention "sample/train_tanda_17_${data[$i]}.csv" "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}"
+    python sample/visualization.py "17_${data[$i]}" "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}"
+    mv "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}/matrices" "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}/matrices_epochs100"
+    mv "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}/test_tanda_17_${data[$i]}.pdf" "Experimentos/TV_APE_seed_$1/exp_${exps[$i]}/test_17_${data[$i]}_epochs100.pdf"
+done
